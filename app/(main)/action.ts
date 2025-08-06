89808df34791087c3b732b9f3dda475076d59718
chya-chya <cktndus32@naver.com>
2025-08-06 11:46:05 +0900

    fix:콘솔로그 제거 및 불필요 파일 제거

--- app/(main)/action.ts
+++ app/(main)/action.ts
@@ -19,9 +19,8 @@ export const getMe = async (): Promise<ActionResult<User>> => {
       success: '회원 정보 조회 성공',
       data: user,
       error: null,
-    }
+    };
   } catch (error) {
-    console.log(error);
     return {
       success: null,
       error:
